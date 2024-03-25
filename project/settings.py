92c5c876b7232c3028403e6aba2bd1459351c552
Blackjoker52 <zishankadri9@gmail.com>
2024-03-25 23:24:29 +0530

    final revision

--- project/settings.py
+++ project/settings.py
@@ -113,7 +113,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # PostegreSQL database (Local)
 
-if not DEV_ENVIROMENT:
+if DEV_ENVIROMENT:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
